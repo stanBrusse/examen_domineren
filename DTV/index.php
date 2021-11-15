a0c68292fe04949297293da4cda4473b38c028ef
Noabs <noah.huijskes@student.graafschapcollege.nl>
2021-11-15 16:40:22 +0100

    Update index.php

--- DTV/index.php
+++ DTV/index.php
@@ -1,10 +1,30 @@
-<style>
-    button{
-        background-color: green;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        body{
+            background-color: indigo;
+            height: 2000px;
+        }
+        .container{
+            position: relative;
+            min-height: 100%;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+<?php 
+include('header.php');
+?>
 
-<button>hallo</button>
-
-<?php
-echo "test";
+<?php 
+include('footer.php');
+?>
+</div>
+</body>
+</html>
