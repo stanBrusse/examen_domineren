468ee3692de7b45c8dfce0319c74ac020569eaf7
Sandbomber2000 <briantv120@gmail.com>
2021-11-15 16:50:20 +0100

    Added kantine php file
    Added kantine.php to test Article card

---
+++ DTV/kantine.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Kantine</title>
+</head>
+
+<body>
+    <header>
+        <?php include('header.php'); ?>
+    </header>
+    <main>
+        <div class="item_Card" style="width: 300px; height: 400px;">
+            <div class="item_Card-Top">
+                <div class="item_Card-Image" style="height: 250px; width: 300px;">
+                    <div class="item_Card-Identifier" style="height: 150px; width: 150px; top:0; left:0;">
+                        SNACK
+                    </div>
+                </div>
+            </div>
+            <div class="item_Card-Bottom">
+
+            </div>
+        </div>
+    </main>
+    <footer>
+    <?php include('footer.php'); ?>
+    </footer>
+</body>
+
+</html>
