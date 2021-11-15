369db93480dd39dcbdf16c633c52b95035cb524c
Noabs <noah.huijskes@student.graafschapcollege.nl>
2021-11-15 16:39:18 +0100

    header & footer

---
+++ DTV/footer.php
@@ -0,0 +1,73 @@
+<style>
+.footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+}
+
+p{    
+  float: right;
+  margin-top: 50px;
+  font-size: 12px;
+  margin-right: 10px;
+}
+
+.footer a{
+  text-decoration: none;
+  font-size: 18px;
+  margin-right: 10px;
+}
+.menu a{    
+    color: cornflowerblue;
+}
+.menu a:hover{
+  color: white;
+}
+.menu{
+    float: left;
+}
+.info{
+    margin-left: 500px;
+}
+.footer ul{
+    list-style: none;
+}
+</style>    
+
+<div class="footer">
+    <div class="menu">
+    <ul>
+        <li>
+    <a class="active" href="#home">Home</a>
+        </li>
+        <li>
+    <a href="#banen">Banen Reserveren</a>
+        </li>
+        <li>
+    <a href="#toernooien">Toernooien</a>
+        </li>
+        <li>
+    <a href="#kantine">Kantine</a>
+        </li>
+        <li>
+    <a href="#inlog">Inloggen</a>
+        </li>
+    </ul>
+    </div>
+    <div class="info">
+        <ul>
+            <li>
+                EMAIL: Contact@DTV.nl
+            </li>
+            <li>
+                TEL: 06-12345678
+            </li>
+            <li>
+                LOCATIE: 7000 GE, DOETINCHEM, Doetinchemse straat 4
+            </li>
+        </ul>
+    </div>
+  <p>Geprogammeerd door Achterhoek Programming AP</p>
+</div>
