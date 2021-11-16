018205da0aff714eb93279c82fd3d91ff2b4244d
Sandbomber2000 <briantv120@gmail.com>
2021-11-16 13:20:48 +0100

    update to Kantine and footer css
    edit to kantine and the css of footer since it was in the way

--- DTV/kantine.php
+++ DTV/kantine.php
@@ -7,29 +7,30 @@
 <head>
     <meta charset="utf-8">
     <title>Kantine</title>
+    <link rel="stylesheet" href="css/kantine.css">
 </head>
 
 <body>
-    <header>
         <?php include('header.php'); ?>
-    </header>
-    <main>
-        <div class="item_Card" style="width: 300px; height: 400px;">
+    <main style="margin-top: 150px; margin-bottom: 163px;">
+
+        <div class="kantine-item-card">
             <div class="item_Card-Top">
-                <div class="item_Card-Image" style="height: 250px; width: 300px;">
-                    <div class="item_Card-Identifier" style="height: 150px; width: 150px; top:0; left:0;">
-                        SNACK
+                <div class="img-container">
+                    <div class="identifier">
+                        <span>SNACK</span>
                     </div>
                 </div>
             </div>
-            <div class="item_Card-Bottom">
-
+            <div class="card-info">
+                <p>Naam: <span>snacker decker</span></p>
+                <p>Prijs: <span>$99,99</span></p>
+                <p>Category: <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quibusdam commodi quas, veniam qui, cupiditate distinctio tempora totam laudantium necessitatibus voluptate, cumque delectus eveniet sequi! Explicabo inventore beatae sapiente minima!</span></p>
             </div>
         </div>
+        
     </main>
-    <footer>
     <?php include('footer.php'); ?>
-    </footer>
 </body>
 
 </html>
