3c598a6c259a8d21565118412cc4929a1faa50af
Sandbomber2000 <briantv120@gmail.com>
2021-11-17 12:11:51 +0100

    Map structure

---
+++ DTV/pages/contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        body{
+            background-color: white;
+        }
+
+        .H1-Contact {
+         font-family: "Arial Black";
+        }
+
+    </style>
+</head>
+<body>
+<?php
+//include('header.php');
+?>
+<h1 class="H1-Contact">CONTACT | DTV</h1>
+
+<p>CONTACT | KEI-FIT
+    Hier vind je de contactgegevens van onze Tennis Club waar jij terecht kunt voor advies, coaching, ondersteuning, inspanning, ontspanning, plezier, uitdaging en gezelligheid. Wij zien én helpen je graag. Heb je een vraag? Stuur ons gerust een bericht. Wij reageren doorgaans binnen 24 uur. Tot ziens bij DTV!
+</p>
+
+<?php
+//include('footer.php');
+?>
+</body>
+</html>
