9818fc17047b52d910072423fe16ae7365c1941c
stanBrusse <stan.brusse@student.graafschapcollege.nl>
2021-11-17 15:49:40 +0100

    update profiel.php
    profiel.php aangepast

--- DTV/pages/profiel.php
+++ DTV/pages/profiel.php
@@ -1,127 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>profiel</title>
     <!-- <link rel="stylesheet" href="../css/inloggen.css"> -->
-    <style> 
+    <style>
+        body {
+            color: white;
+        }
 
-body
-{
-    color: white;
-}
-button
-{
-    width: 75px;
-    height: 30px;
-}
-td
-{
-    justify-content: center;
-    text-align: center;
-}
-.A_afmelden{
-    padding: 2px;
-    background-color: white;
-    border: 1px solid black;
-    text-decoration: none;
-    color: black;
-}
+        button {
+            width: 75px;
+            height: 30px;
+        }
 
-</style>
+        td {
+            justify-content: center;
+            text-align: center;
+        }
+
+        .A_afmelden {
+            padding: 2px;
+            background-color: white;
+            border: 1px solid black;
+            text-decoration: none;
+            color: black;
+        }
+    </style>
 </head>
+
 <body>
-<?php
+    <?php
     include('header.php');
-?>
+    ?>
 
 
-<div class="cont">
-    <div class="containerlinks">
-        <form action="#uitloggen">
-        <input type="submit" value="uitloggen"><br><br>
-    </form>    
-    gereservereerde banen
-    <table border="1">
-        <tr>
-            <th> baan</th>
-            <th> tijd</th>
-            <th> afmelden</th>
-        </tr>
-    
-        <tr>
-            <td class="td1"> Binnenbaan 3</td>
-            <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td2"> butenbaanbaan 3</td>
-            <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td1"> Binnenbaan 3</td>
-            <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td2"> Binnenbaan 3</td>
-            <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td1"> Binnenbaan 3</td>
-            <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td2"> Binnenbaan 3</td>
-            <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        <tr>
-            <td class="td1"> Binnenbaan 3</td>
-            <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
-            <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-        
-    </table> <br>
-    ingeschreven voor toernooien
-    <table border="1">
-        <tr>
-            <th> toernooi naam  </th>
-            <th> datum          </th>
-            <td>afmelden</td>
-        </tr>
-    
-        <tr>
-            <td class="td1"> toernooi       </td>
-            <td class="td1"> 18-11-2021 <br> 12:00 - 14:00  </td>
-            <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-    
-        <tr>
-            <td class="td2"> toernooi </td>
-            <td class="td2"> 18-11-2021 <br> 12:00 - 14:00  </td>
-            <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
-        </tr>
-    </table>
-</div>
-    <div class="containerrechts">
-    <h4>wachtwoord veranderen</h4>
-    <form action="profiel.php" method="POST">   
-        <input type="text" placeholder="wachtwoord"><br><br>
-        <input type="password" placeholder="wachtwoord herhalen"><br><br>
-        <input type="submit" value="verander wachtwoord"class="buttonInloggen">
-    </form>
+    <div class="cont">
+        <div class="containerlinks">
+            
+            gereservereerde banen
+            <table border="1">
+                <tr>
+                    <th> baan</th>
+                    <th> tijd</th>
+                    <th> afmelden</th>
+                </tr>
+
+                <tr>
+                    <td class="td1"> Binnenbaan 3</td>
+                    <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td2"> butenbaanbaan 3</td>
+                    <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td1"> Binnenbaan 3</td>
+                    <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td2"> Binnenbaan 3</td>
+                    <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td1"> Binnenbaan 3</td>
+                    <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td2"> Binnenbaan 3</td>
+                    <td class="td2"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+                <tr>
+                    <td class="td1"> Binnenbaan 3</td>
+                    <td class="td1"> 18-11-2021 <br> 15:00- 16:00</td>
+                    <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+
+            </table> <br>
+            ingeschreven voor toernooien
+            <table border="1">
+                <tr>
+                    <th> toernooi naam </th>
+                    <th> datum </th>
+                    <td>afmelden</td>
+                </tr>
+
+                <tr>
+                    <td class="td1"> toernooi </td>
+                    <td class="td1"> 18-11-2021 <br> 12:00 - 14:00 </td>
+                    <td class="td1"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+
+                <tr>
+                    <td class="td2"> toernooi </td>
+                    <td class="td2"> 18-11-2021 <br> 12:00 - 14:00 </td>
+                    <td class="td2"><a href="profiel.php" class="A_afmelden">afmelden</a></td>
+                </tr>
+            </table>
+        </div>
+        <div class="containerrechts">
+            <form action="#uitloggen">
+                <input type="submit" value="uitloggen"><br><br>
+            </form>
+            <h4>wachtwoord veranderen</h4>
+            <form action="profiel.php" method="POST">
+                <input type="text" placeholder="wachtwoord"><br><br>
+                <input type="password" placeholder="wachtwoord herhalen"><br><br>
+                <input type="submit" value="verander wachtwoord" class="buttonInloggen">
+            </form>
+        </div>
+
     </div>
 
-</div>
-
-<?php
+    <?php
     include('footer.php');
-?>
+    ?>
 </body>
+
 </html>
