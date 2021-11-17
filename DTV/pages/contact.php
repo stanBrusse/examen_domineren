58350036058d52231d0f75f14601c215c0617683
Wurmpy <94183752+Wurmpy@users.noreply.github.com>
2021-11-17 13:36:23 +0100

    Update contact.php

--- DTV/pages/contact.php
+++ DTV/pages/contact.php
@@ -5,29 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <style>
-        body{
-            background-color: white;
-        }
-
-        .H1-Contact {
-         font-family: "Arial Black";
-        }
-
-    </style>
 </head>
 <body>
 <?php
-//include('header.php');
+include('header.php');
 ?>
-<h1 class="H1-Contact">CONTACT | DTV</h1>
+<section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
 
-<p>CONTACT | KEI-FIT
-    Hier vind je de contactgegevens van onze Tennis Club waar jij terecht kunt voor advies, coaching, ondersteuning, inspanning, ontspanning, plezier, uitdaging en gezelligheid. Wij zien én helpen je graag. Heb je een vraag? Stuur ons gerust een bericht. Wij reageren doorgaans binnen 24 uur. Tot ziens bij DTV!
-</p>
+    <div class="bg-overlay"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-8 col-md-10 mx-auto col-12">
+                <div class="hero-text mt-5 text-center">
+
+                    <h1 class="text-white">CONTACT | DTV</h1>
+
+                    <h6 =>CONTACT | KEI-FIT
+                        Hier vind je de contactgegevens van onze Tennis Club waar jij terecht kunt voor advies, coaching, ondersteuning, inspanning, ontspanning, plezier, uitdaging en gezelligheid. Wij zien én helpen je graag. Heb je een vraag? Stuur ons gerust een bericht. Wij reageren doorgaans binnen 24 uur. Tot ziens bij DTV!</h6>
+
+                    <a href="#feature" class="btn custom-btn mt-3" data-aos="fade-up" data-aos-delay="600">Get started</a>
+
+                    <a href="#about" class="btn custom-btn bordered mt-3" data-aos="fade-up" data-aos-delay="700">learn more</a>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 <?php
-//include('footer.php');
+include('footer.php');
 ?>
 </body>
 </html>
