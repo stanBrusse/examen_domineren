3ee7943bdfbedfefd67809fda53adfbaee687dc9
Sandbomber2000 <briantv120@gmail.com>
2021-11-17 15:45:34 +0100

    updated Kantine to use External JS file

--- DTV/js/kantine.js
+++ DTV/js/kantine.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
-function kantineFilter() {
-    var x = document.getElementById("kantineSelect").value;
-    if(x == "all"){
-        document.getElementById('snack').style.display = 'block';
-        document.getElementById('drink').style.display = 'block';
-    }
-    if(x == "snack"){
-        document.getElementById('snack').style.display = 'block';
-        document.getElementById('drink').style.display = 'none';
-    }
-    if(x == "drink"){
-        document.getElementById('drink').style.display = 'block';
-        document.getElementById('snack').style.display = 'none';
-    }
-  }
+$(document).ready(function() {
+    let filter = document.getElementById('kantineSelect');
+    let snacks = document.getElementsByClassName('snack-card');
+    let drinks = document.getElementsByClassName('drink-card');
 
+    filter.onchange = function() {
+        let x = document.getElementById("kantineSelect").value;
+        console.log(snacks[1]);
+        console.log(drinks[0]);
+
+        if (x == "all") {
+            for(let i = 0; i < snacks.length; i++){console.log(snacks[i]); snacks[i].style.display = 'block';}
+            for(let i = 0; i < drinks.length; i++){console.log(drinks[i]); drinks[i].style.display = 'block';}
+            
+        }
+        if (x == "snack") {
+            for(let i = 0; i < snacks.length; i++){snacks[i].style.display = 'block';}
+            for(let i = 0; i < drinks.length; i++){drinks[i].style.display = 'none';}
+        }
+        if (x == "drink") {
+            for(let i = 0; i < snacks.length; i++){snacks[i].style.display = 'none';}
+            for(let i = 0; i < drinks.length; i++){drinks[i].style.display = 'block';}
+        }
+    }
 });
