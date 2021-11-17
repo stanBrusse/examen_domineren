af0f9635ea084d0cbb850b714d59c52c23ab16ff
Sandbomber2000 <briantv120@gmail.com>
2021-11-17 15:01:24 +0100

    Kantine works

--- DTV/pages/kantine.php
+++ DTV/pages/kantine.php
@@ -11,15 +11,19 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
+            let filter = document.getElementById('kantineSelect');
             let snacks = document.getElementsByClassName('snack-card');
             let drinks = document.getElementsByClassName('drink-card');
 
             filter.onchange = function() {
                 let x = document.getElementById("kantineSelect").value;
+                console.log(snacks[1]);
+                console.log(drinks[0]);
 
                 if (x == "all") {
-                    for(let i = 0; i < snacks.length; i++){snacks[i].style.display = 'block';}
-                    for(let i = 0; i < drinks.length; i++){drinks[i].style.display = 'block';}
+                    for(let i = 0; i < snacks.length; i++){console.log(snacks[i]); snacks[i].style.display = 'block';}
+                    for(let i = 0; i < drinks.length; i++){console.log(drinks[i]); drinks[i].style.display = 'block';}
+                    
                 }
                 if (x == "snack") {
                     for(let i = 0; i < snacks.length; i++){snacks[i].style.display = 'block';}
@@ -259,7 +263,7 @@
             </div>
             
         </div>
-        
+
     <?php include('footer.php'); ?>
 
 </body>
