7d7764344df90d6f826d3775c20d80a2db50999b
stanBrusse <stan.brusse@student.graafschapcollege.nl>
2021-11-18 12:36:29 +0100

    Update profiel.php

--- DTV/pages/profiel.php
+++ DTV/pages/profiel.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>profiel</title>
-    <!-- <link rel="stylesheet" href="../css/inloggen.css"> -->
+    <link rel="stylesheet" href="../css/inloggen.css">
     <style>
         body {
             color: white;
