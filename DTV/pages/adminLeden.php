2d82a192eebc0aeb6c292f179042b7bdc225e103
stanBrusse <stan.brusse@student.graafschapcollege.nl>
2021-11-19 13:57:03 +0100

    ledenlijst voor de admin + css aangepast

---
+++ DTV/pages/adminLeden.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leden</title>
+<style>
+    table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  text-align: center;
+  padding: 8px;
+
+}
+th{
+    background-color: #f13a11;
+}
+tr:nth-child(even) {background-color: lightgray;}
+.hero
+{
+    padding-top: 0px;
+}
+input[type=text] {
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+}
+input[type=submit] {
+    width: 200px;
+    height: 50px;
+    background-color: lightskyblue;
+    border: 1px solid lightskyblue;
+    margin-top: 140px;
+}
+
+    </style>
+</head>
+<body>
+<?php 
+include('header.php');
+?>
+<div class="container">
+
+<section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
+<form action="adminLeden.php" method="POST">
+    <input type="text" placeholder="zoek op voornaam">
+    <input type="submit" placeholder="zoek">
+</form>
+<table >
+    <thead>    
+        <th>lidnummer</th>
+        <th>voornaam</th>
+        <th>achternaam</th>
+        <th>email</th>
+        <th>adres</th>
+        <th>telnummer</th>
+    </thead>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+    </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+    <tr>
+        <td>1234</td>
+        <td>voornaam</td>
+        <td>achternaam</td>
+        <td>voornaam.achternaam@gmail.com</td>
+        <td>straatnaam 2 <br>doetinchem</td>
+        <td>06 12345678</td>
+     </tr>
+</table>
+</section>
+<?php 
+include('footer.php');
+?>
+</div>
+</body>
+</html>
