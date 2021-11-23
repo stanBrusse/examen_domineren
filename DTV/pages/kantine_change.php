878ed003070793d306f515516488526ad538486b
Sandbomber2000 <briantv120@gmail.com>
2021-11-23 12:47:43 +0100

    Kantine Change + increased Wachtwoord size DB

--- DTV/pages/kantine_change.php
+++ DTV/pages/kantine_change.php
@@ -18,7 +18,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $stmt = $conn->query("SELECT * FROM `artikelen` WHERE `nummer`=" . $nummer . "");
     $result = $stmt->fetch();
     var_dump($result);
+    $naam = $result["naam"];
+    $prijs = $result["prijs"];
+    $foto = $result["foto"];
+    $categorie = $result["categorie"];
+    $descriptie = $result["descriptie"];
 } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     if (empty($_POST["naam"])) {
         $naamErr = "Naam is required";
     } else {
@@ -48,70 +54,40 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     }
     $target_dir = "../images/";
     $target_file = $target_dir . basename($_FILES["foto"]["name"]);
-    $uploadOk = 1;
+
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["foto"]["tmp_name"]);
-        if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
 
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-
-    // Check file size
-    if ($_FILES["foto"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-
-    // Allow certain file formats
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-
+    $uploadOk = 1;
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
         // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
-            echo "The file " . htmlspecialchars(basename($_FILES["foto"]["name"])) . " has been uploaded.";
-            try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                $sql = "UPDATE `artikelen` SET naam=?, descriptie=?, foto=?, prijs=?, categorie=? WHERE nummer=?";
-                $stmt = $pdo->prepare($sql);
-                $nummer = (int) $_POST['nummer'];
-                $stmt->execute([$naam, $descriptie, $foto, $prijs, $categorie, $nummer]);
+            $sql = "UPDATE artikelen SET naam=?, descriptie=?, foto=?, prijs=?, categorie=? WHERE nummer=?";
+            $stmt = $conn->prepare($sql);
+            $nummer = (int) $_POST['nummer'];
+            $foto = "images/" . basename($_FILES["foto"]["name"]);
+            $stmt->execute([$naam, $descriptie, $foto, $prijs, $categorie, $nummer]);
 
-                echo "Record updated successfully";
-            } catch (PDOException $e) {
-                echo $sql . "<br>" . $e->getMessage();
-            }
+            echo "Record updated successfully";
+
             $naam = $prijs = $foto = $categorie = $descriptie = "";
             $conn = null;
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+            header("location:kantine.php");
+            exit;
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
         }
     }
 }
+
+
 function test_input($data)
 {
     $data = trim($data);
@@ -128,7 +104,6 @@ function test_input($data)
     <title>Kantine Create</title>
     <link rel="stylesheet" href="../css/kantine.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="../js/kantine.js"></script>
     <!--IF USER IS ADMIN THIS SHOWS-->
     <?php include('kantine_admin.php') ?>
 </head>
@@ -147,11 +122,11 @@ function test_input($data)
             <textarea id="descriptie" name="descriptie" rows="5" cols="40" maxlength="55" required><?php echo $descriptie; ?></textarea><br />
 
             <label for="foto"><strong>Item Foto</strong></label>
-            <input type="file" id="foto" name="foto" required><br />
+            <input type="file" id="foto" name="foto"><br />
             <span id="foto" name="foto"><img src="../<?php echo $foto; ?>"></span><br />
 
             <label for="prijs"><strong>Item Prijs</strong></label>
-            <input type="doubleval" id="prijs" name="prijs" required value="<?php echo $prijs;?>"><br />
+            <input type="doubleval" id="prijs" name="prijs" required value="<?php echo $prijs; ?>"><br />
 
             <label for="categorie"><strong>Item categorie</strong></label>
             <span id="categorie" name="categorie"><?php echo $categorie; ?></span><br />
