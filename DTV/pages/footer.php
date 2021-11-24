358fef604655f0d837847aa65f621dec1a724c0d
Wurmpy <94183752+Wurmpy@users.noreply.github.com>
2021-11-24 16:04:15 +0100

    Update footer.php

--- DTV/pages/footer.php
+++ DTV/pages/footer.php
@@ -22,3 +22,8 @@
         </div>
     </div>
 </footer>
+<script src="../js/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/aos.js"></script>
+<script src="../js/smoothscroll.js"></script>
+<script src="../js/custom.js"></script>
