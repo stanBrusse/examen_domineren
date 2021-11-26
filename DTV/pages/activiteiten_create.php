dcafeb44e243206af707dc8350b0b75d38b4bf78
Sandbomber2000 <briantv120@gmail.com>
2021-11-26 16:31:47 +0100

    activiteit creation plus typo fix banen

--- DTV/pages/activiteiten_create.php
+++ DTV/pages/activiteiten_create.php
@@ -20,59 +20,62 @@ $activiteiten = $stmt;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin banen</title>
-  <style>
-    .cont {
-      margin-top: 150px;
-      position: relative;
-      color: black;
-      border: 1px solid black;
-    }
-  </style>
+  <title>Activiteit Create</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../css/activiteiten_admin.css">
 </head>
 
 <body>
   <?php
   include('header.php');
   ?>
-  <section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
 
 
-    <div class="container">
-      <div class="row">
+  <div class="activiteiten-container">
+    <div id="activiteiten-create">
+      <form class="activiteiten-form" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
 
-        <div class="col-lg-8 col-md-10 mx-auto col-12">
-          <div class="hero-text mt-5 text-center">
+        <section class="section-activiteit">
+          <label for="activiteit">Activiteit:</label><br>
+          <label for="radio-1">Toernooi</label>
+          <input class="mdc-radio__native-control" type="radio" id="radio-1" name="activiteit" checked>
+          <label for="radio-2">Wedstrijd</label>
+          <input class="mdc-radio__native-control" type="radio" id="radio-2" name="activiteit">
+          <label for="radio-3">Training</label>
+          <input class="mdc-radio__native-control" type="radio" id="radio-3" name="activiteit"><br>
+        </section>
 
-            <div class="cont">
-              <p>Banen reserveren</p>
-              <hr>
-              <p>Datum</p>
-              <input type="date">
-              <p>Van - Tot</p>
-              <input type="time">
-              <input type="time"><br>
-              <input class="mdc-radio__native-control" type="radio" id="radio-1" name="radios" checked>
-              <label for="radio-1">Toernooi</label>
-              <input class="mdc-radio__native-control" type="radio" id="radio-2" name="radios">
-              <label for="radio-2">Wedstrijd</label>
-              <input class="mdc-radio__native-control" type="radio" id="radio-3" name="radios">
-              <label for="radio-3">Training</label><br>
-              <input class="mdc-radio__native-control" type="radio" id="radio-4" name="radios2" checked>
-              <label for="radio-4">Buiten</label>
-              <input class="mdc-radio__native-control" type="radio" id="radio-5" name="radios2">
-              <label for="radio-5">Binnen</label>
-              <p>Aantal banen</p>
-              <input type="text">
-              <p>Aantal werknemers</p>
-              <input type="text"><br><br>
-              <input type="submit" value="Toevoegen">
-            </div>
-          </div>
+        <section class="section-title">
+          <label for="title">Title:</label>
+          <input type="text" name="title"><br>
+        </section>
 
-        </div>
-  </section>
+        <section class="section-datum_activiteit">
+          <label for="datum_activiteit">Datum van:</label>
+          <input type="date" name="datum_activiteit"><br>
+        </section>
 
+        <section class="section-datum_ingeschreven">
+          <label for="datum_ingeschreven">Leden Maximaal Inschrijven</label>
+          <input type="date" name="datum_ingeschreven"><br>
+        </section>
+
+        <section class="section-tijd_start">
+          <label for="tijd_start">Start:</label>
+          <input type="time" name="tijd_start"><br>
+        </section>
+
+        <section class="section-tijd_eind">
+          <label for="tijd_start">Eind:</label>
+          <input type="time" name="tijd_eind"><br>
+        </section>
+
+        <section class="section-submit">
+          <input type="submit" value="Toevoegen">
+        </section>
+      </form>
+    </div>
+  </div>
 
 
   <?php
