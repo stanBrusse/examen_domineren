447ea90102544d8f979c2d961d21dac7fd16eb24
stanBrusse <stan.brusse@student.graafschapcollege.nl>
2021-12-01 15:20:21 +0100

    Update toernooiDetail.php
    terug knop

--- DTV/pages/toernooiDetail.php
+++ DTV/pages/toernooiDetail.php
@@ -1,7 +1,7 @@
 <?php
     include('header.php');
 
-
+$info = "";
 if(!isset($_SESSION['lidnummer']))
 {
     if (headers_sent()) {
@@ -29,25 +29,27 @@ if (isset($_GET['nummer'])) {
         }
     }
 }elseif(isset($_GET['aanmelden']) &&isset($_GET['toernooinummer']) && isset($_GET['id'])){
-    $check = $pdo->prepare("SELECT * FROM registratie_activiteit WHERE activiteit_nummer=? AND lid_nummer=?");
-    $check->bindParam(1, $_GET['toernooinummer']);
-    $check->bindParam(2, $_GET['id']);
-    $check->execute();
-    if($check->rowCount() == 0)
+    $aantaldeelnemers = $pdo->query("select count(*) FROM registratie_activiteit WHERE activiteit_nummer=".$_GET['toernooinummer']."")->fetchColumn(); 
+    if($aantaldeelnemers <=32)
     {
-        $insertActiviteit = $pdo->prepare("INSERT INTO registratie_activiteit  (activiteit_nummer, lid_nummer, datum_inschrijfing) VALUES( ?, ? , ?)");
-        $insertActiviteit->bindParam(1, $_GET['toernooinummer']);
-        $insertActiviteit->bindParam(2, $_GET['id']);
-        $insertActiviteit->bindParam(3, $date);
-        $insertActiviteit->execute();
+            $insertActiviteit = $pdo->prepare("INSERT INTO registratie_activiteit  (activiteit_nummer, lid_nummer, datum_inschrijfing) VALUES( ?, ? , ?)");
+            $insertActiviteit->bindParam(1, $_GET['toernooinummer']);
+            $insertActiviteit->bindParam(2, $_GET['id']);
+            $insertActiviteit->bindParam(3, $date);
+            $insertActiviteit->execute();
+        
+        if (headers_sent()) {
+            die("Redirect failed. Please click on this link: <a href=../pages/toernooien.php");
+        }
+        else{
+            exit(header("location:toernooien.php"));
+        }
+    }else{echo '<script type="text/javascript">'; 
+        echo 'alert("maximaal aantal inschrijvingen al behaald. u kunt niet aanmelden");'; 
+        echo 'window.location.href = "toernooien.php";';
+        echo '</script>';
     }
-    if (headers_sent()) {
-        die("Redirect failed. Please click on this link: <a href=../pages/toernooien.php");
-    }
-    else{
-        exit(header("location:toernooien.php"));
-    }
-    echo "fsdadsfadsfasdfadasffads"; 
+    
 }else{
     if (headers_sent()) {
         die("Redirect failed. Please click on this link: <a href=../pages/toernooien.php");
@@ -95,6 +97,17 @@ if (isset($_GET['nummer'])) {
             width: 150px;
             height: 50px;
         }
+        .terugknop{
+            background-color: tomato;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            float: right;
+            width: 100%;
+            text-align: center;
+        }
     </style>
 </head>
 
@@ -102,9 +115,11 @@ if (isset($_GET['nummer'])) {
     
     <section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
 
-
         <div class="cont">
+        <a href="toernooien.php"><button class="terugknop">terug</button></a><br><br><br>
+            
             <table>
+
         <?php   foreach($selectActiviteit as $row)
                 { 
                     echo "<tr>";
@@ -117,26 +132,16 @@ if (isset($_GET['nummer'])) {
                     echo "<td> het toernooi begint om ".$row['tijd_start'] . " en eindigd om " .$row['tijd_eind'] . "<br> het toernooi is op: " . $row['datum_activiteit'] ."</td>";
                     echo "</tr>";                     
                     echo "<tr>";
-                    $sql = $pdo->prepare("SELECT * FROM activiteit_werknemer WHERE activiteit_nummer=?");
-                    $sql->bindParam(1, $_GET['nummer']);
-                    $sql->execute(); 
+                    $aantaldeelnemers = $pdo->query("select count(*) FROM registratie_activiteit WHERE activiteit_nummer=".$_GET['nummer']."")->fetchColumn(); 
                     echo "<td>";  
-                    foreach($sql as $row)
-                    {
-                        $sql = $pdo->prepare("SELECT * FROM accounts WHERE nummer=?");
-                        $sql->bindParam(1, $row['werknemer_nummer']);
-                        $sql->execute();  
-                        foreach($sql as $row2)
-                        {
-                            echo $row['rol'] . " - " . $row2['naam_voor'] . " " . $row2['naam_tussen'] . " " . $row2['naam_achter'] . "<br>";
-                        }
-                    }                  
+                    echo "aantal deelnemers: $aantaldeelnemers/32 ";             
                     echo "</td";
                     echo "</tr>";   
                     echo "<tr>";                     
                     echo "<td><a href='toernooiDetail.php?aanmelden=true&toernooinummer=".$_GET['nummer']."&id=".$_SESSION['lidnummer']." '><button>aanmelden</button></a></td>";
                     echo "</tr>"; 
                  }
+                 echo $info;
 ?>            </table>
 
     </section>
